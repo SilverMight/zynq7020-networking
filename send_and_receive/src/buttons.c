c76c51effcb0b11b53eb647bf89a45ecf34cab7d
SilverMight <joeyzee1234@gmail.com>
2024-03-01 15:44:56 -0500

    Add debug button print statement, update button state

--- send_and_receive/src/buttons.c
+++ send_and_receive/src/buttons.c
@@ -1,4 +1,5 @@
 #include "buttons.h"
+#include "xil_printf.h"
 
 uint32_t buttonState = 0;
 
@@ -10,10 +11,14 @@ WandaError process_buttons(uint32_t command) {
 
 	for(int button = 0; button < WANDA_NUM_BUTTONS; button++) {
 		if(updatedButtons & (1 << button)) {
-
+			xil_printf("Button %d is activated!\n", button);
 		}
 	}
 
+	// TODO: logic to make sure button state is OK
+	buttonState = updatedButtons;
+
+	return WANDA_ERR_OK;
 }
 
 
